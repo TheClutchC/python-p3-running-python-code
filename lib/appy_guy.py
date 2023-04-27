9a68485c40ff0f3c969223153125b05362a8aa8d
TheClutchC <claycarney7@gmail.com>
2023-04-26 23:30:43 -0400

    Python Fundamentals 1.1

---
+++ lib/appy_guy.py
@@ -0,0 +1,33 @@
+# lib/app.py
+print("Hello World!")
+print("Hello sun!")
+print("Hello sky!")
+
+x=0
+x=2
+x=4
+print(x)
+
+# <--TIPS-->
+# Run single line of code above in Python Shell with 'shift + enter'
+
+# <--ACCESSING PYTHON SHELL-->
+# Enter Python Shell in bash terminal with 'python'
+# Exit Python Shell in bash terminal with 'ctrl + d' or 'exit()'
+
+# <--NAMING VARIABLES-->
+# Use under_scores for naming variables, also known as snake case \
+# As opposed to using camelCase in JS
+
+# <--INSTALLING DEPENDENCIES IN PIPFILE-->
+# Type in bash terminal 'pipenv install'
+# This creates a .venv file, or virtual environment
+
+# <--ACCESSING PIPENV SHELL-->
+# Type in bash terminal 'pipenv shell'
+# This lets you access the created virtual environment
+
+# <--TESTING WITH PYTEST-->
+# Must be in virtual environment 
+# Type in pipenv shell 'pytest'
+# If multiple test fail, use 'pytest -x' to exit after first fail for easier debugging
